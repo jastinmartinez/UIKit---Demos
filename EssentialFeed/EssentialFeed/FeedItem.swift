754893f975c85dad991b494a0a05873be7d03664
Jastin Martinez <martinez.jastin@hotmail.com>
2022-11-20 13:15:50 -0400

    folder

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jastin Martínez on 20/11/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let desription: String?
+    let location: String?
+    let imageURL: String?
+}
