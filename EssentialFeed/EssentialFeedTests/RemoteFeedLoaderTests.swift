5b1d56a614b0c6b365b06646326e2d84d4dc4acc
Jastin Martinez <jastin.martinez@caden.io>
2022-11-24 19:59:38 -0400

    make the httpClient a protocol also remove weak func dependency and instead use init injection

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,18 +8,39 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let client: HTTPClient
 
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func load() {
+        self.client.get(from: URL(string: "wwwaaa")!)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_init() {
-        let client = HTTPClient()
-        let sut = RemoteFeedLoader()
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClientSpy()
         XCTAssertNil(client.requestedURL)
+    }
+
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client)
+        sut.load()
+        XCTAssertNotNil(client.requestedURL)
     }
 }
